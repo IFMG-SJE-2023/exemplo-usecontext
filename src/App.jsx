a20cd47b04eb49efcd82247956379fb2ca21d01f
LucianoCB <lucianocab@g>
2023-05-08 13:12:31 -0300

    codigo encerado para caber em slides

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,35 @@
-/* eslint-disable no-unused-vars */
-import { useState ,  useContext, createContext } from 'react';
+import { useState , createContext } from 'react';
 import './App.css';
 import Footer from './Footer';
 import { Autenticacao } from './Contexts';
 
-
 export const Debugar = createContext('nao');
-
- 
-
 
 function App() {
   //  formulario
   const [username, setUsername] = useState(0);
+  function handleInput(event){ setUsername(event.target.value); }
 
   // simulando um login
   const [user, setUser] = useState({ 'id': null  , 'nome': '' });
 
-  
-  function handleInput(event){
-    setUsername(event.target.value);
-  }
-  
   function handleLogin(){
-    
-    // busca login no banco de dados 
-    // ...
+    // busca login no banco de dados  ...
     setUser ( { 'id': 123  , 'nome': username } );
-
   }
 
   return (
-    
     <>
-
       <h1>Exemplo de uso do hook useContext</h1>
       <div className="card">
-          <input  type="text"  name="login" value={username} onChange={handleInput} />
-          <button onClick={handleLogin}>
-            logar
-          </button>
+          <input  type="text"  name="login" 
+            value={username} onChange={handleInput} />
+          <button onClick={handleLogin}> logar</button>
       </div>
-
     <Debugar.Provider value="tudo">
-    <Autenticacao.Provider value={user}>
-
-      <Footer />
-    </Autenticacao.Provider >
+      <Autenticacao.Provider value={user}>
+        <Footer />
+      </Autenticacao.Provider >
     </Debugar.Provider>
     </>
   )
--- src/Footer/index.jsx
+++ src/Footer/index.jsx
@@ -1,26 +1,15 @@
-/* eslint-disable no-unused-vars */
 import { useContext } from "react";
 import { Debugar} from '../App.jsx'
 import { Autenticacao } from '../Contexts.js';
 export default function Footer(){
-
-    // contextos separados num arquivo 
-    let usuariologado = useContext(Autenticacao);
-    
-    // contexto criados num arquivo pai
-    const debugar = useContext(Debugar);
-    
+    let usuariologado = useContext(Autenticacao);  // contextos separados num arquivo 
+    const debugar = useContext(Debugar); // contexto criados num arquivo pai
     return(<>
-    
         <p>
             devemos debugar {debugar}.
-
             <br />
-            o usuario é {usuariologado.nome} de ID: {usuariologado.id}
+            o usuario é {usuariologado.nome} de 
+            ID: {usuariologado.id}
         </p>
-        
-    
-    
     </>)
-
 }
